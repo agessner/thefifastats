3b5af39c9ab616a4acce18aba1170633b8fc30e2
Airton Jeferson Gessner <airton@mercos.com>
2020-08-10 22:52:59 -0300

    creating Tags component

---
+++ post-generator/src/Tags.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { TextareaAutosize } from '@material-ui/core';
+
+export default class Tags extends React.Component {
+    render() {
+        return <TextareaAutosize rowsMax={12}>
+            #fifa #ultimateteam #soccer #lovesoccer #football #ea #easports #careermodefifa #fifa20 #fifa21 #futebol #jogador #futbol #foxsports #espn #fifastats #data #datascience #sports
+        </TextareaAutosize>
+
+    }
+}
