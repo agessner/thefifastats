e508378fc491936206998c35bb3372b3cc4b4b81
Airton Jeferson Gessner <airton@mercos.com>
2020-08-19 23:48:21 -0300

    creating squad post

--- post-generator/src/App.js
+++ post-generator/src/App.js
@@ -8,6 +8,7 @@ import ListItemText from '@material-ui/core/ListItemText';
 import {Hidden, List, ListItem, ListItemIcon, Drawer, AppBar, Divider} from "@material-ui/core";
 import { makeStyles } from '@material-ui/core/styles';
 import {Top3Players} from "./Top3Players";
+import Squad from "./Squad";
 
 function ListItemLink(props) {
   const { icon, primary, to } = props;
@@ -92,7 +93,8 @@ function App() {
                     [
                         ['home', 'Home'],
                         ['player-evolution', 'Player Evolution'],
-                        ['top-3-players', 'Top 3 Players']
+                        ['top-3-players', 'Top 3 Players'],
+                        ['squad', 'Squad']
                     ].map((text, index) => (
                     <ListItemLink to={`/${text[0]}`} primary={text[1]} />
                 ))}
@@ -120,6 +122,7 @@ function App() {
                     <Switch>
                             <Route path="/top-3-players"><Top3Players/></Route>
                             <Route path="/player-evolution"><PlayerEvolution/></Route>
+                            <Route path="/squad"><Squad/></Route>
                             <Route path="/"><Home/></Route>
                     </Switch>
                 </Router>
--- post-generator/src/gateways.js
+++ post-generator/src/gateways.js
@@ -18,4 +18,21 @@ function getTopPlayersAtVersionAndPosition(version, playerPosition) {
     return fetch(`http://127.0.0.1:5000/comparasion/${version}/${playerPosition}`).then(res => res.json()).then(result => result['result'])
 }
 
-export default { loadPlayers, getPlayer, loadVersions, loadPositions, getTopPlayersAtVersionAndPosition }
+function loadTeams(url) {
+    return fetch(`http://127.0.0.1:5000/${url}/`).then(res => res.json()).then(result => result['result'])
+}
+
+function loadTeamPlayers(worstOrBest, postType, team) {
+    const url = `${worstOrBest}/${postType}/${team}`
+    return fetch(`http://127.0.0.1:5000/${url}/`).then(res => res.json()).then(result => result['result'])
+}
+
+export default {
+    loadPlayers,
+    getPlayer,
+    loadVersions,
+    loadPositions,
+    getTopPlayersAtVersionAndPosition,
+    loadTeams,
+    loadTeamPlayers
+}
