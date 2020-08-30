080a522581c7ce8fea13f0a6683a6d3823c096e2
Airton Jeferson Gessner <airton@mercos.com>
2020-08-30 19:51:33 -0300

    fixing gateways and creating new function

--- post-generator/src/gateways.js
+++ post-generator/src/gateways.js
@@ -1,11 +1,12 @@
 const loadPlayers = () => get(`players/`)
 const getPlayer = playerId => get(`player/${playerId}`)
-const loadVersions = () => get() (`versions/`)
-const loadPositions = () => get() (`player-positions/`)
+const loadVersions = () => get(`versions/`)
+const loadPositions = () => get(`player-positions/`)
 const getTopPlayersAtVersionAndPosition = (version, playerPosition, field) => get(`${version}/${playerPosition}/${field}`)
 const loadTeams = url => get(`${url}/`)
 const loadTeamPlayers = (worstOrBest, postType, team) => get(`${worstOrBest}/${postType}/${team}/`)
 const loadCombinedTeamPlayers = (team1, team2) => get(`${team1}/${team2}`)
+const loadBestVersionTeam = version => get(`version-team/${version}/`)
 
 function get(url) {
     return fetch(`http://127.0.0.1:5000/${url}`).then(res => res.json()).then(result => result['result'])
@@ -19,5 +20,6 @@ export default {
     getTopPlayersAtVersionAndPosition,
     loadTeams,
     loadTeamPlayers,
-    loadCombinedTeamPlayers
+    loadCombinedTeamPlayers,
+    loadBestVersionTeam
 }
