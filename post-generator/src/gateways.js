ba7737c16585eebe2fca29144e6a134faaeea94a
Airton Jeferson Gessner <airton@mercos.com>
2020-09-05 13:27:44 -0300

    fixing gateways and creating new url

--- post-generator/src/gateways.js
+++ post-generator/src/gateways.js
@@ -2,10 +2,11 @@ const loadPlayers = () => get(`players/`)
 const getPlayer = playerId => get(`player/${playerId}`)
 const loadVersions = () => get(`versions/`)
 const loadPositions = () => get(`player-positions/`)
-const getTopPlayersAtVersionAndPosition = (version, playerPosition, field) => get(`${version}/${playerPosition}/${field}`)
+const getTopPlayersAtVersionAndPosition = (version, playerPosition, field) => get(`comparasion/${version}/${playerPosition}/${field}`)
+const getTopPlayersAtVersionByAttribute = (version, attribute) => get(`comparasion/${version}/${attribute}`)
 const loadTeams = url => get(`${url}/`)
 const loadTeamPlayers = (worstOrBest, postType, team) => get(`${worstOrBest}/${postType}/${team}/`)
-const loadCombinedTeamPlayers = (team1, team2) => get(`${team1}/${team2}`)
+const loadCombinedTeamPlayers = (team1, team2) => get(`combined-teams/${team1}/${team2}`)
 const loadBestVersionTeam = version => get(`version-team/${version}/`)
 
 function get(url) {
@@ -18,6 +19,7 @@ export default {
     loadVersions,
     loadPositions,
     getTopPlayersAtVersionAndPosition,
+    getTopPlayersAtVersionByAttribute,
     loadTeams,
     loadTeamPlayers,
     loadCombinedTeamPlayers,
