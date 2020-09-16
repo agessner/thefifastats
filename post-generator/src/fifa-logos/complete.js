becd33399ddc639e9df0dcea96e017a06182d156
Airton Jeferson Gessner <airton@mercos.com>
2020-09-16 20:53:31 -0300

    adding complete.js

---
+++ post-generator/src/fifa-logos/complete.js
@@ -0,0 +1,58 @@
+export const logos = {
+    'FIFA 07': {
+        'src': 'https://cdn.steamgriddb.com/logo/1d075e2fdbc8e2e66a47b39a0652ce31.png',
+        'width': '35%'
+    },
+    'FIFA 08': {
+        'src': 'https://cdn.steamgriddb.com/logo/8ef899fe33f78d18506d037fd734f900.png',
+        'width': '25%'
+    },
+    'FIFA 09': {
+        'src': 'https://images.launchbox-app.com/2f62e99f-0ab2-4e87-ab74-ddfb3c56d8c0.png',
+        'width': '35%'
+    },
+    'FIFA 10': {
+        'src': 'https://i.neoseeker.com/mgv/255404/404/2/fifa10_head_display.png',
+        'width': '32%'
+    },
+    'FIFA 11': {
+        'src': 'https://www.logolynx.com/images/logolynx/07/07372f100b10edcbc717a845321ffb6f.jpeg',
+        'width': '25%'
+    },
+    'FIFA 12': {
+        'src': 'https://www.ps3blog.ch/wp-content/uploads/2011/07/FIFA12logoTEXTrgb-1024x355.jpg',
+        'width': '25%'
+    },
+    'FIFA 13': {
+        'src': 'https://www.mobilegamer.com.br/wp-content/uploads/2012/06/FIFA-13.png',
+        'width': '25%'
+    },
+    'FIFA 14': {
+        'src': 'https://www.gametactics.com/wp-content/uploads/2013/06/FIFA-14-LOGO-SMALL.jpg',
+        'width': '31%'
+    },
+    'FIFA 15': {
+        'src': 'https://lh3.googleusercontent.com/proxy/lR72uFN55W2dMftnPHMZYPEn90T9XxZaD8vS0HzZ2V2D3h5lF3o8Xv5tANwFVIat5b_2O98ALpY9fAtUWG7xYQylT_kd7vox8MtEPSe6_EE',
+        'width': '35%'
+    },
+    'FIFA 16': {
+        'src': 'https://blognicematin.typepad.com/.a/6a0120a864ed46970b01bb08782aa6970d-600wi',
+        'width': '27%'
+    },
+    'FIFA 17': {
+        'src': 'https://www.fifplay.com/img/public/ea-sports-fifa-17-logo.png',
+        'width': '35%'
+    },
+    'FIFA 18': {
+        'src': '../fifa-logos/fifa-18-complete.png',
+        'width': '35%'
+    },
+    'FIFA 19': {
+        'src': 'https://www.fifplay.com/img/public/fifa-19-logo-black.png',
+        'width': '35%'
+    },
+    'FIFA 20': {
+        'src': 'https://cdn131.picsart.com/305054538099211.png?type=webp&to=min&r=640',
+        'width': '31%'
+    },
+}
